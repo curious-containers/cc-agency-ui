3524572d838949e3f3b5f37d14a43c31ba1f8e92
Dominik Probst <mail@d-probst.de>
2023-11-09 13:05:15 +0000

    add icons; show version

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -10,40 +10,46 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
           <li class="nav-item">
             <router-link :to="{ name: 'Dashboard' }">
-              <p>Dashboard</p>
+              <i class="nav-icon fas fa-home"></i>
+              Dashboard
             </router-link>
           </li>
           <li class="nav-item">
             <router-link :to="{ name: 'Experiments' }">
-              <p>Experiments</p>
+              <i class="nav-icon fas fa-flask"></i>
+              Experiments
             </router-link>
           </li>
           <li class="nav-item">
             <router-link :to="{ name: 'Batches' }">
-              <p>Batches</p>
-            </router-link>
-          </li>
-          <!-- <li class="nav-item">
-            <router-link to="/">
-              <p>Connector</p>
+              <i class="nav-icon fas fa-th"></i>
+              Batches
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/">
-              <p>Cloud</p>
+            <router-link :to="{ name: 'Nodes' }">
+              <i class="nav-icon fas fa-share-nodes"></i>
+              Nodes
             </router-link>
-          </li> -->
+          </li>
         </ul>
       </nav>
     </div>
     <!-- /.sidebar -->
     <div class="sidebar-footer-content">
-      <nav class="mt-2">
+      <nav class="mt-2 nav-footer">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
           <li class="nav-item">
-            <a class="text-white" href="" @click.prevent="handleLogout">Logout</a>
+            <a class="text-white" href="" @click.prevent="handleLogout">
+              <i class="nav-icon fas fa-user"></i>
+              Logout
+            </a>
           </li>
         </ul>
+        <li class="nav-item">
+          <p class="text-white">UI Version: {{ uiVersion }}</p>
+          <p class="text-white">CC-Agency Version: {{ agencyVersion }}</p>
+        </li>
       </nav>
     </div>
   </aside>
@@ -51,12 +57,30 @@
 
 <script>
 import AuthService from '@/services/auth'
+import api from '@/services/api'
+import packageInfo from '../../package.json';
 
 export default {
   name: 'Sidebar',
+  data() {
+    return {
+      uiVersion: packageInfo.version,
+      agencyVersion: undefined
+    }
+  },
+  mounted() {
+    this.loadAgencyVersion()
+  },
   methods: {
     handleLogout() {
       AuthService.logout()
+    },
+    loadAgencyVersion() {
+      api.get('/version').then(res => {
+        this.agencyVersion = res.data.agencyVersion
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
@@ -71,5 +95,21 @@ export default {
   position: absolute;
   height: fit-content;
   bottom: 0px;
+}
+
+nav {
+  padding-left: 20px !important;
+}
+
+nav p {
+  font-weight: bold;
+}
+
+.nav-item {
+  margin-bottom: 1.5rem !important;
+}
+
+.nav-footer {
+  padding-bottom: 0px;
 }
 </style>
